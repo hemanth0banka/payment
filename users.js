6367cc946b5a82cf256491df773ce2b65da1d7da
hemanth0banka <150692843+hemanth0banka@users.noreply.github.com>
2025-10-15 20:59:17 +0530

    Add files via upload

--- users.js
+++ users.js
@@ -14,6 +14,15 @@ const users = sequelize.define('users',{
     password : {
         type : DataTypes.STRING,
         allowNull : false
+    },
+    primeuser : {
+        type : DataTypes.BOOLEAN,
+        allowNull : true
+    },
+    total : {
+        type : DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
     }
 })
 module.exports = users
